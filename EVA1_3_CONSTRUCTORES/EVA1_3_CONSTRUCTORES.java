7c6febd08599f3f00013e542b362cfec4794cb39
paola gallegos <124700531+paolagaaol@users.noreply.github.com>
2023-02-09 09:49:44 -0600

    Add files via upload

---
+++ EVA1_3_CONSTRUCTORES/EVA1_3_CONSTRUCTORES.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1_2_clases_java;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA1_2_CLASES_JAVA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // clase es la receta
+        // objeto es el platillo 
+        // 1. Declarar el identificador para el objeto
+        // 2. Asignar memoria con new
+        // 3. Invocar su constructor
+        // Persona () -> constructor
+        // constructor --> método
+        
+        Persona perso1 = new Persona ();
+        /* ESTA SECCION ES PORQUE INICIAMOS CON LOS
+        /ATRIBUTOS CON MODIFICADOR DEFAULT
+        
+        perso1.nombre = "Paola";
+        perso1.apellido = "Gallegos";
+        perso1.edad = 18;
+        perso1.estadoCivil = false;
+        
+        System.out.println (perso1.nombre);
+        System.out.println (perso1.apellido);
+        System.out.println (perso1.edad);
+        System.out.println (perso1.estadoCivil);
+        */
+         
+        perso1.setNombre("Paola ");
+        perso1.setApellido("Gallegos");
+        perso1.setEdad(18);
+        perso1.setEstadoCivil(true);
+        
+        /*System.out.println(perso1.getNombre());
+        System.out.println(perso1.getApellido());
+        System.out.println(perso1.getEdad());
+        System.out.println(perso1.getEstadoCivil());
+        */
+        perso1.imprimirDatos();
+        
+        System.out.println ("---------");
+        
+        //Persona 2
+         Persona perso2 = new Persona ();
+        perso2.setNombre("Cesar");
+        perso2.setApellido("Urias");
+        perso2.setEdad(19);
+        perso2.setEstadoCivil(true);
+        
+                perso2.imprimirDatos();
+
+    }
+    
+}
